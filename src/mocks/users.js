812a71cd1c4d863ecebd62bf077d29b6bacc0440
Estefan Hense <30737971+Estefangh@users.noreply.github.com>
2023-06-24 08:22:31 -0300

    First commit of API
    Add index, controllers and mocks

---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Estefan'
+    },
+    {
+        id: 2,
+        name: 'Michel'
+    },
+    {
+        id: 3,
+        name: 'Milton'
+    },
+    {
+        id: 4,
+        name: 'Neiva'
+    }
+]
